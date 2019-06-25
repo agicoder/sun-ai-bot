937f9f50d4a91c700f6a050f253e4ed4a9faf067
agicoder@TrungTuyen dev <trungtuyen.dev@gmail.com>
2019-06-25 21:42:38 +0700

    add google tts

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const api = "AIzaSyC6PmNlQSpLaT3x_TuKV4Cueidv1HwAGNU";
 var googleTranslate = require('google-translate')(api);
+// Imports the Google Cloud client library
+const textToSpeech = require('@google-cloud/text-to-speech');
+
+// Import other required libraries
+const fs = require('fs');
+const util = require('util');
 
 app.get('/', function(req, res) {
     res.render('index.ejs');
@@ -13,12 +19,38 @@ io.sockets.on('connection', function(socket) {
    
     socket.on('chat_message', function(message) {
         io.emit('chat_message_me', message)
+        create_mp3(message)
+        
+
         googleTranslate.translate(message, 'vi', function(err, translation) {
-        io.emit('chat_message_bot', translation.translatedText);})
+            io.emit('chat_message_bot', translation.translatedText);})
     });
 
 });
 
+async function create_mp3(message) {
+
+  // Creates a client
+  const client = new textToSpeech.TextToSpeechClient();
+
+  // The text to synthesize
+  const text = message;
+
+  // Construct the request
+  const request = {
+    input: {text: text},
+    // Select the language and SSML Voice Gender (optional)
+    voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
+    // Select the type of audio encoding
+    audioConfig: {audioEncoding: 'MP3'},
+  };
+  // Performs the Text-to-Speech request
+  const [response] = await client.synthesizeSpeech(request);''
+  // Write the binary audio content to a local file
+  const writeFile = util.promisify(fs.writeFile);
+  await writeFile('output.mp3', 'response'.audioContent, 'binary');
+  console.log('Audio content written to file: output.mp3');
+}
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
